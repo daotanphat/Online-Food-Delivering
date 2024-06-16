2409c7ff2bb122f447f3433352c6c34fa9cbe7eb
Dao Tan Phat <daotanphat2143@gmail.com>
2024-06-16 10:24:15 +0700

    feat: init project

---
+++ src/main/java/com/phat/food_delivering/FoodDeliveringApplication.java
@@ -0,0 +1,13 @@
+package com.phat.food_delivering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodDeliveringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodDeliveringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/phat/food_delivering/FoodDeliveringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phat.food_delivering;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodDeliveringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
