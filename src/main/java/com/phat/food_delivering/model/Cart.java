7cc40359a32cb7fc31b55bf31c12346b05c8a761
Dao Tan Phat <daotanphat2143@gmail.com>
2024-06-18 23:18:32 +0700

    feat: create Cart entity

---
+++ src/main/java/com/phat/food_delivering/model/Cart.java
@@ -0,0 +1,28 @@
+package com.phat.food_delivering.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Cart {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @OneToOne
+    private User customer;
+
+    @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<CartItem> items = new ArrayList<>();
+
+    private Long total;
+}
