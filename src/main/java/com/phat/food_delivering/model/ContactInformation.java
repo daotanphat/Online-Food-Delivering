6e92092115a3fda33a3d00d23ffe2cfcf06426b9
Dao Tan Phat <daotanphat2143@gmail.com>
2024-06-18 23:15:55 +0700

    feat: create class ContactInformation include informations related to restaurant

---
+++ src/main/java/com/phat/food_delivering/model/ContactInformation.java
@@ -0,0 +1,11 @@
+package com.phat.food_delivering.model;
+
+import lombok.Data;
+
+@Data
+public class ContactInformation {
+    private String email;
+    private String mobile;
+    private String twitter;
+    private String instagram;
+}
