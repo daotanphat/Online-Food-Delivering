4f7ab626e302e1bd56f97212c74d49437f0c3a24
Dao Tan Phat <daotanphat2143@gmail.com>
2024-06-18 23:13:29 +0700

    feat: create Address class

---
+++ src/main/java/com/phat/food_delivering/model/Address.java
@@ -0,0 +1,20 @@
+package com.phat.food_delivering.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+}
