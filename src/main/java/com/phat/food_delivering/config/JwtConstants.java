bfb8244289faaf49a70b5350e3259f7478b2f990
Dao Tan Phat <daotanphat2143@gmail.com>
2024-06-19 23:44:29 +0700

    feat: add file have constant variables

---
+++ src/main/java/com/phat/food_delivering/config/JwtConstants.java
@@ -0,0 +1,7 @@
+package com.phat.food_delivering.config;
+
+public class JwtConstants {
+    public static final String SECRET_KEY = "bQeThWmZq4t7w!z$C&F)J@NcRfUjXn2r5u8x/A?D*G-KaPdSgVkYp3s6v9y$B&E)";
+    public static final String HEADER = "Authorization";
+    public static final String BEARER = "Bearer";
+}
