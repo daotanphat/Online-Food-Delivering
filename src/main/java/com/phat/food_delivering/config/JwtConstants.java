c57bb5678c23a10c298a4adff5ec4be71b281c40
Dao Tan Phat <daotanphat2143@gmail.com>
2024-06-20 16:02:52 +0700

    refactor: add constant variable token expiration time

--- src/main/java/com/phat/food_delivering/config/JwtConstants.java
+++ src/main/java/com/phat/food_delivering/config/JwtConstants.java
@@ -4,4 +4,5 @@ public class JwtConstants {
     public static final String SECRET_KEY = "bQeThWmZq4t7w!z$C&F)J@NcRfUjXn2r5u8x/A?D*G-KaPdSgVkYp3s6v9y$B&E)";
     public static final String HEADER = "Authorization";
     public static final String BEARER = "Bearer";
+    public static final int TOKEN_EXPIRATION = 24 * 60 * 60 * 1000;
 }
