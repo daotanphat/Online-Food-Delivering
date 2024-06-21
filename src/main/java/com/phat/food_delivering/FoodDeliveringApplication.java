66ecf3fac1ef2d3bfd5ec9d1ae7e426f28400511
Dao Tan Phat <daotanphat2143@gmail.com>
2024-06-21 23:02:41 +0700

    refactor: add bean encode password

--- src/main/java/com/phat/food_delivering/FoodDeliveringApplication.java
+++ src/main/java/com/phat/food_delivering/FoodDeliveringApplication.java
@@ -2,6 +2,9 @@ package com.phat.food_delivering;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class FoodDeliveringApplication {
@@ -10,4 +13,8 @@ public class FoodDeliveringApplication {
         SpringApplication.run(FoodDeliveringApplication.class, args);
     }
 
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
