2c82fdfa5d8882ecf11fa476912fb4b5083f2b2e
Dao Tan Phat <daotanphat2143@gmail.com>
2024-06-22 14:53:55 +0700

    feat: add class to handle exception is token is invalid

---
+++ src/main/java/com/phat/food_delivering/security/filter/ExceptionHandlerFilter.java
@@ -0,0 +1,22 @@
+package com.phat.food_delivering.security.filter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+public class ExceptionHandlerFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        try {
+            filterChain.doFilter(request, response);
+        } catch (RuntimeException e) {
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            response.getWriter().write("Bad request");
+            response.getWriter().flush();
+        }
+    }
+}
