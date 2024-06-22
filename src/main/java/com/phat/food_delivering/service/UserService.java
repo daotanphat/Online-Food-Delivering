cadcd69147932baaacda4ed0b3b5d8c68d455bf3
Dao Tan Phat <daotanphat2143@gmail.com>
2024-06-22 22:28:59 +0700

    refactor: add method find user based on token & find all users

--- src/main/java/com/phat/food_delivering/service/UserService.java
+++ src/main/java/com/phat/food_delivering/service/UserService.java
@@ -2,8 +2,12 @@ package com.phat.food_delivering.service;
 
 import com.phat.food_delivering.model.User;
 
+import java.util.List;
+
 public interface UserService {
     User findUserByEmail(String email);
 
     User saveUser(User user);
+    User findUserBasedOnToken(String token);
+    List<User> findAllUsers();
 }
