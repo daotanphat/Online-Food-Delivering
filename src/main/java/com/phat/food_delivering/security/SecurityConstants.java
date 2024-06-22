d58ccd4cf8046cf38ab1900b46569354d333b251
Dao Tan Phat <daotanphat2143@gmail.com>
2024-06-22 14:50:11 +0700

    refactor: add variable path to sign up

--- src/main/java/com/phat/food_delivering/security/SecurityConstants.java
+++ src/main/java/com/phat/food_delivering/security/SecurityConstants.java
@@ -1,8 +1,9 @@
-package com.phat.food_delivering.config;
+package com.phat.food_delivering.security;
 
-public class JwtConstants {
+public class SecurityConstants {
     public static final String SECRET_KEY = "bQeThWmZq4t7w!z$C&F)J@NcRfUjXn2r5u8x/A?D*G-KaPdSgVkYp3s6v9y$B&E)";
     public static final String HEADER = "Authorization";
-    public static final String BEARER = "Bearer";
+    public static final String BEARER = "Bearer ";
     public static final int TOKEN_EXPIRATION = 24 * 60 * 60 * 1000;
+    public static final String AUTH_PATH = "/auth/signup";
 }
