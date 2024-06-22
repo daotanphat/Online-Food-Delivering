af725350d712787e0f442c57ced29f9c4530aaa3
Dao Tan Phat <daotanphat2143@gmail.com>
2024-06-22 22:28:24 +0700

    feat: implement method to find All User & method to return information of current user sign in

--- src/main/java/com/phat/food_delivering/service/UserServiceIplm.java
+++ src/main/java/com/phat/food_delivering/service/UserServiceIplm.java
@@ -1,10 +1,18 @@
 package com.phat.food_delivering.service;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.Claim;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import com.phat.food_delivering.model.User;
 import com.phat.food_delivering.repository.UserRepository;
+import com.phat.food_delivering.security.SecurityConstants;
+import io.jsonwebtoken.Claims;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class UserServiceIplm implements UserService {
@@ -24,5 +32,20 @@ public class UserServiceIplm implements UserService {
     public User saveUser(User user) {
         user.setPassword(passwordEncoder.encode(user.getPassword()));
         return userRepository.save(user);
+    }
+
+    @Override
+    public User findUserBasedOnToken(String token) {
+        DecodedJWT subject = JWT.require(Algorithm.HMAC512(SecurityConstants.SECRET_KEY))
+                .build()
+                .verify(token);
+        String email = subject.getClaim("email").asString();
+        User user = userRepository.findByEmail(email);
+        return user;
+    }
+
+    @Override
+    public List<User> findAllUsers() {
+        return userRepository.findAll();
     }
 }
