d787adc3dd8ac8e8d97a89ad68c459ea9827c2fc
Dao Tan Phat <daotanphat2143@gmail.com>
2024-06-22 14:52:57 +0700

    feat: handle when user login it return token

---
+++ src/main/java/com/phat/food_delivering/security/filter/AuthenticationFilter.java
@@ -0,0 +1,72 @@
+package com.phat.food_delivering.security.filter;
+
+import com.auth0.jwt.algorithms.Algorithm;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.phat.food_delivering.model.User;
+import com.phat.food_delivering.security.SecurityConstants;
+import com.phat.food_delivering.security.manager.CustomAuthenticationManager;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import com.auth0.jwt.JWT;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.io.IOException;
+import java.util.*;
+
+@AllArgsConstructor
+public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+    private CustomAuthenticationManager authenticationManager;
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+        try {
+            User user = new ObjectMapper().readValue(request.getInputStream(), User.class);
+            Authentication authentication = new UsernamePasswordAuthenticationToken(user.getEmail(), user.getPassword());
+            return authenticationManager.authenticate(authentication);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
+        Collection<? extends GrantedAuthority> authorities = authResult.getAuthorities();
+        String roles = populateAuthorities(authorities);
+
+        String token = JWT.create()
+                .withClaim("email", (String) authResult.getPrincipal())
+                .withClaim("authorities", roles)
+                .withExpiresAt(new Date(System.currentTimeMillis() + SecurityConstants.TOKEN_EXPIRATION))
+                .sign(Algorithm.HMAC512(SecurityConstants.SECRET_KEY));
+        System.out.println(token);
+        response.addHeader(SecurityConstants.HEADER, SecurityConstants.BEARER + token);
+    }
+
+    private String populateAuthorities(Collection<? extends GrantedAuthority> authorities) {
+        Set<String> auths = new HashSet<>();
+
+        for (GrantedAuthority authority : authorities) {
+            auths.add((authority.getAuthority()));
+        }
+        return String.join(",", auths);
+    }
+
+    @Override
+    protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response, AuthenticationException failed) throws IOException, ServletException {
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.getWriter().write(failed.getMessage());
+        response.getWriter().flush();
+    }
+}
