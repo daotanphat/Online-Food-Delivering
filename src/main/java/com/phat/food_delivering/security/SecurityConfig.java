dc32bf7201c8f7d3c61d4b95ab0285f09279d42f
Dao Tan Phat <daotanphat2143@gmail.com>
2024-06-22 14:49:47 +0700

    refactor: remove bean password encode

--- src/main/java/com/phat/food_delivering/security/SecurityConfig.java
+++ src/main/java/com/phat/food_delivering/security/SecurityConfig.java
@@ -1,17 +1,19 @@
-package com.phat.food_delivering.config;
+package com.phat.food_delivering.security;
 
 import com.phat.food_delivering.model.USER_ROLE;
+import com.phat.food_delivering.security.filter.ExceptionHandlerFilter;
+import com.phat.food_delivering.security.filter.JwtAuthorizationFilter;
+import com.phat.food_delivering.security.manager.CustomAuthenticationManager;
 import jakarta.servlet.http.HttpServletRequest;
+import com.phat.food_delivering.security.filter.AuthenticationFilter;
+import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 
@@ -20,15 +22,25 @@ import java.util.Collections;
 
 @Configuration
 @EnableWebSecurity
-public class AppConfig {
+@AllArgsConstructor
+public class SecurityConfig {
+    private final CustomAuthenticationManager customAuthenticationManager;
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        AuthenticationFilter authenticationFilter = new AuthenticationFilter(customAuthenticationManager);
+        authenticationFilter.setFilterProcessesUrl("/login");
+
         http.sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("api/admin/**").hasAnyRole(String.valueOf(USER_ROLE.ROLE_ADMIN), String.valueOf(USER_ROLE.ROLE_RESTAURANT_OWNER))
+                        .requestMatchers("api/admin/**").hasAnyRole(String.valueOf(USER_ROLE.ADMIN), String.valueOf(USER_ROLE.RESTAURANT_OWNER))
                         .requestMatchers("api/**").authenticated()
+                        .requestMatchers(HttpMethod.POST, SecurityConstants.AUTH_PATH).permitAll()
                         .anyRequest().permitAll())
-                .addFilterBefore(new JwtTokenValidator(), UsernamePasswordAuthenticationFilter.class)
+                //.addFilterBefore(new JwtTokenValidator(), UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(new ExceptionHandlerFilter(), AuthenticationFilter.class)
+                .addFilter(authenticationFilter)
+                //.addFilterAfter(new JwtAuthorizationFilter(), AuthenticationFilter.class)
                 .csrf(csrf -> csrf.disable())
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()));
         return http.build();
@@ -49,10 +61,5 @@ public class AppConfig {
                 return config;
             }
         };
-    }
-
-    @Bean
-    PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
     }
 }
