f436323a6427f05def5c1e105aea763c184c2f03
Dao Tan Phat <daotanphat2143@gmail.com>
2024-06-24 00:37:30 +0700

    refactor: add annotation Repository

--- src/main/java/com/phat/food_delivering/repository/CartRepository.java
+++ src/main/java/com/phat/food_delivering/repository/CartRepository.java
@@ -2,6 +2,8 @@ package com.phat.food_delivering.repository;
 
 import com.phat.food_delivering.model.Cart;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CartRepository extends JpaRepository<Cart, Long> {
 }
