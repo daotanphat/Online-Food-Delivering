90d5a744e9f3da19a7bade1ea60ee33711d269fb
Dao Tan Phat <daotanphat2143@gmail.com>
2024-06-24 00:38:43 +0700

    refactor: change return value Authentication

--- src/main/java/com/phat/food_delivering/security/filter/JwtAuthorizationFilter.java
+++ src/main/java/com/phat/food_delivering/security/filter/JwtAuthorizationFilter.java
@@ -1,6 +1,7 @@
 package com.phat.food_delivering.security.filter;
 
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import com.phat.food_delivering.security.SecurityConstants;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
@@ -8,12 +9,16 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 import com.auth0.jwt.JWT;
 
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
 
 public class JwtAuthorizationFilter extends OncePerRequestFilter {
     @Override
@@ -21,6 +26,7 @@ public class JwtAuthorizationFilter extends OncePerRequestFilter {
         String header = request.getHeader(SecurityConstants.HEADER);
         if (header == null || !header.startsWith(SecurityConstants.BEARER)) {
             filterChain.doFilter(request, response);
+            return;
         }
 
         String token = header.replace(SecurityConstants.BEARER, "");
@@ -28,7 +34,14 @@ public class JwtAuthorizationFilter extends OncePerRequestFilter {
                 .build()
                 .verify(token)
                 .getSubject();
-        Authentication authentication = new UsernamePasswordAuthenticationToken(user, null, Arrays.asList());
+        DecodedJWT jwt = JWT.require(Algorithm.HMAC512(SecurityConstants.SECRET_KEY))
+                .build()
+                .verify(token);
+        String email = jwt.getClaim("email").asString();
+        String role = jwt.getClaim("authorities").asString();
+        GrantedAuthority authority = new SimpleGrantedAuthority(role);
+        Collection<? extends GrantedAuthority> authorities = Collections.singletonList(authority);
+        Authentication authentication = new UsernamePasswordAuthenticationToken(email, null, authorities);
         SecurityContextHolder.getContext().setAuthentication(authentication);
         filterChain.doFilter(request, response);
     }
