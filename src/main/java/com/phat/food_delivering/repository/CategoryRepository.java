22fc1a368be77db997288dd3b77f2a433914d470
Dao Tan Phat <daotanphat2143@gmail.com>
2024-06-25 22:10:33 +0700

    feat: add CategoryRepository

---
+++ src/main/java/com/phat/food_delivering/repository/CategoryRepository.java
@@ -0,0 +1,20 @@
+package com.phat.food_delivering.repository;
+
+import com.phat.food_delivering.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+    List<Category> findByRestaurantId(Long restaurantId);
+
+    @Query("SELECT c FROM Category c" +
+            " WHERE c.name LIKE '%:search%'")
+    List<Category> getCategoriesBySearch(@Param("search") String search);
+}
