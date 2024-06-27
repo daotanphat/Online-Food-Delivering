cc361df1fc7fcb3d033f57870071e11893245e82
Dao Tan Phat <daotanphat2143@gmail.com>
2024-06-27 16:30:50 +0700

    feat: add cart item repository

---
+++ src/main/java/com/phat/food_delivering/repository/CartItemRepository.java
@@ -0,0 +1,9 @@
+package com.phat.food_delivering.repository;
+
+import com.phat.food_delivering.model.CartItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CartItemRepository extends JpaRepository<CartItem, Long> {
+}
