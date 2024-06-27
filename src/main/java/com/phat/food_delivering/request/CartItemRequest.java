1a470fbd1177f2ce8bb7c88a4cf51960e6b7020f
Dao Tan Phat <daotanphat2143@gmail.com>
2024-06-27 16:30:37 +0700

    feat: add dto cart item request

---
+++ src/main/java/com/phat/food_delivering/request/CartItemRequest.java
@@ -0,0 +1,12 @@
+package com.phat.food_delivering.request;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class CartItemRequest {
+    private Long foodId;
+    private int quantity;
+    private List<String> ingredients;
+}
