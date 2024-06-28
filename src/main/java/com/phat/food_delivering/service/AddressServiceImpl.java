f742a2264ffec605eaa6504537ea7a98bd29051b
Dao Tan Phat <daotanphat2143@gmail.com>
2024-06-28 21:38:06 +0700

    refactor: implement logic method getAddresses, findAddress and checkDuplicatedAddress

--- src/main/java/com/phat/food_delivering/service/AddressServiceImpl.java
+++ src/main/java/com/phat/food_delivering/service/AddressServiceImpl.java
@@ -5,6 +5,8 @@ import com.phat.food_delivering.repository.AddressRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class AddressServiceImpl implements AddressService {
 
@@ -14,5 +16,36 @@ public class AddressServiceImpl implements AddressService {
     @Override
     public Address save(Address address) {
         return addressRepository.save(address);
+    }
+
+    @Override
+    public List<Address> getAddresses() {
+        return addressRepository.findAll();
+    }
+
+    @Override
+    public boolean checkDuplicateAddress(Address address, List<Address> addresses) {
+        for (Address address1 : addresses) {
+            if (address1.getStreetAddress().equalsIgnoreCase(address.getStreetAddress())
+                    && address1.getCity().equalsIgnoreCase(address.getCity())
+                    && address1.getStateProvince().equalsIgnoreCase(address.getStateProvince())
+                    && address1.getPostalCode().equals(address.getPostalCode())
+                    && address1.getCountry().equalsIgnoreCase(address.getCountry())
+            ) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public Address findAddress(Address address) {
+        return addressRepository.findByStreetAddressAndCityAndStateProvinceAndPostalCodeAndCountry(
+                address.getStreetAddress(),
+                address.getCity(),
+                address.getStateProvince(),
+                address.getPostalCode(),
+                address.getCountry()
+        );
     }
 }
