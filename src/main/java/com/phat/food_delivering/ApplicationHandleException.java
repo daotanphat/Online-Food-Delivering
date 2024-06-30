0b37380ebf947117263fb1ff447cd22b26482aed
Dao Tan Phat <daotanphat2143@gmail.com>
2024-06-30 16:56:43 +0700

    feat: add method to handle exception not found list entity

--- src/main/java/com/phat/food_delivering/ApplicationHandleException.java
+++ src/main/java/com/phat/food_delivering/ApplicationHandleException.java
@@ -1,6 +1,7 @@
 package com.phat.food_delivering;
 
 import com.phat.food_delivering.exception.EntityNotFoundException;
+import com.phat.food_delivering.exception.ListEntityNotFoundException;
 import com.phat.food_delivering.response.MessageResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +14,13 @@ public class ApplicationHandleException extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler({EntityNotFoundException.class})
     public ResponseEntity<Object> handleResourceNotfoundException(RuntimeException ex) {
+        MessageResponse messageResponse = new MessageResponse();
+        messageResponse.setMessage(ex.getMessage());
+        return new ResponseEntity<>(messageResponse, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler({ListEntityNotFoundException.class})
+    public ResponseEntity<Object> handleListEntityNotFoundException(RuntimeException ex) {
         MessageResponse messageResponse = new MessageResponse();
         messageResponse.setMessage(ex.getMessage());
         return new ResponseEntity<>(messageResponse, HttpStatus.NOT_FOUND);
