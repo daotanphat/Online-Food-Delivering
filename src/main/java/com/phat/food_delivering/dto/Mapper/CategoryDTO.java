19873f07eeea8ca4a36d769283b834fdf17374f5
Dao Tan Phat <daotanphat2143@gmail.com>
2024-06-30 16:52:29 +0700

    feat: create CategoryDTO and mapper of it

---
+++ src/main/java/com/phat/food_delivering/dto/Mapper/CategoryDTO.java
@@ -0,0 +1,8 @@
+package com.phat.food_delivering.dto.Mapper;
+
+public record CategoryDTO(
+        Long id,
+        String name,
+        String description
+) {
+}
---
+++ src/main/java/com/phat/food_delivering/dto/Mapper/CategoryDTOMapper.java
@@ -0,0 +1,26 @@
+package com.phat.food_delivering.dto.Mapper;
+
+import com.phat.food_delivering.model.Category;
+import com.phat.food_delivering.request.CreateCategoryRequest;
+import org.springframework.stereotype.Service;
+
+import java.util.function.Function;
+
+@Service
+public class CategoryDTOMapper implements Function<Category, CategoryDTO> {
+    @Override
+    public CategoryDTO apply(Category category) {
+        return new CategoryDTO(
+                category.getId(),
+                category.getName(),
+                category.getDescription()
+        );
+    }
+
+    public Category toCategory(CreateCategoryRequest request) {
+        Category category = new Category();
+        category.setName(request.getName());
+        category.setDescription(request.getDescription());
+        return category;
+    }
+}
