d63a3e32dfd7f46190f98bea5bcc75bb731e3231
Dao Tan Phat <daotanphat2143@gmail.com>
2024-07-12 16:10:08 +0700

    feat: add method to find ingredient Item By food id

--- src/main/java/com/phat/food_delivering/repository/IngredientItemRepository.java
+++ src/main/java/com/phat/food_delivering/repository/IngredientItemRepository.java
@@ -2,6 +2,8 @@ package com.phat.food_delivering.repository;
 
 import com.phat.food_delivering.model.IngredientsItem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,4 +11,13 @@ import java.util.List;
 @Repository
 public interface IngredientItemRepository extends JpaRepository<IngredientsItem, Long> {
     List<IngredientsItem> findByRestaurantId(Long restaurantId);
+
+    @Query(
+            nativeQuery = true,
+            value = "SELECT i1.id, i1.in_stock, i1.name, i1.category_id, i1.restaurant_id\n" +
+                    "FROM ingredients_item AS i1\n" +
+                    "JOIN ingredient_food AS i2 ON i1.id = i2.ingredient_id\n" +
+                    "WHERE i2.food_id = :foodId"
+    )
+    List<IngredientsItem> findByFoodId(@Param("foodId") Long foodId);
 }
