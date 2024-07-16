22c2064e664bcfc42d3bc5e692df9238a71edb3b
Dao Tan Phat <daotanphat2143@gmail.com>
2024-07-16 22:53:28 +0700

    refactor: return dto object to user instead of entire object

--- src/main/java/com/phat/food_delivering/controller/Admin/AdminOrderController.java
+++ src/main/java/com/phat/food_delivering/controller/Admin/AdminOrderController.java
@@ -1,5 +1,6 @@
 package com.phat.food_delivering.controller.Admin;
 
+import com.phat.food_delivering.dto.OrderDTO;
 import com.phat.food_delivering.model.Order;
 import com.phat.food_delivering.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,19 +17,19 @@ public class AdminOrderController {
     OrderService orderService;
 
     @PutMapping("/{id}/status")
-    public ResponseEntity<Order> updateOrderStatus(
+    public ResponseEntity<OrderDTO> updateOrderStatus(
             @PathVariable Long id,
             @RequestParam String status
     ) {
-        Order order = orderService.updateOrderStatus(id, status);
-        return new ResponseEntity<>(order, HttpStatus.OK);
+        OrderDTO orderDTO = orderService.updateOrderStatus(id, status);
+        return new ResponseEntity<>(orderDTO, HttpStatus.OK);
     }
 
     @GetMapping("/restaurant/{id}/{status}")
-    public ResponseEntity<List<Order>> getRestaurantOrder(
+    public ResponseEntity<List<OrderDTO>> getRestaurantOrder(
             @PathVariable Long id,
             @PathVariable String status) {
-        List<Order> orders = orderService.getRestaurantOrder(id, status);
-        return new ResponseEntity<>(orders, HttpStatus.OK);
+        List<OrderDTO> orderDTOS = orderService.getRestaurantOrder(id, status);
+        return new ResponseEntity<>(orderDTOS, HttpStatus.OK);
     }
 }
