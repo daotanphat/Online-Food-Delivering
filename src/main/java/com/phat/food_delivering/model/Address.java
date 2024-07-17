676b6ccbe7a4ae2e33a94f0899e4e1b0c0b641cb
Dao Tan Phat <daotanphat2143@gmail.com>
2024-07-17 23:31:03 +0700

    refactor: add method equal & hashcode

--- src/main/java/com/phat/food_delivering/model/Address.java
+++ src/main/java/com/phat/food_delivering/model/Address.java
@@ -8,6 +8,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.Objects;
+
 @Entity
 @Data
 @AllArgsConstructor
@@ -23,4 +25,21 @@ public class Address {
     private String stateProvince;
     private Long postalCode;
     private String country;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Address address = (Address) o;
+        return Objects.equals(streetAddress, address.streetAddress) &&
+                Objects.equals(city, address.city) &&
+                Objects.equals(stateProvince, address.stateProvince) &&
+                Objects.equals(postalCode, address.postalCode) &&
+                Objects.equals(country, address.country);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(streetAddress, city, stateProvince, postalCode, country);
+    }
 }
