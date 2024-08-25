54fae7bc8ae0b9fbb152e8f460f88425fd7c9509
Dao Tan Phat <daotanphat2143@gmail.com>
2024-08-25 23:10:31 +0700

    refactor: change secret key

--- src/main/java/com/phat/food_delivering/security/SecurityConstants.java
+++ src/main/java/com/phat/food_delivering/security/SecurityConstants.java
@@ -1,7 +1,7 @@
 package com.phat.food_delivering.security;
 
 public class SecurityConstants {
-    public static final String SECRET_KEY = "bQeThWmZq4t7w!z$C&F)J@NcRfUjXn2r5u8x/A?D*G-KaPdSgVkYp3s6v9y$B&E)";
+    public static final String SECRET_KEY = "zzKuADl6R1vgbGYESb7meWuGvONSRgJ0Ps3rJZkP1HiYCpnsqu3BcZY2usMPxI8A";
     public static final String HEADER = "Authorization";
     public static final String BEARER = "Bearer ";
     public static final int TOKEN_EXPIRATION = 24 * 60 * 60 * 1000;
